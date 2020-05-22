b64c92c93ce59d2ac87a40efbba2a5f5bdff86b4
Cody Hatfield <cody.hatfield@me.com>
2020-05-21 17:36:57 -0700

    Fix styling

--- ipfs-ios-backup.rb
+++ ipfs-ios-backup.rb
@@ -1,35 +1,31 @@
 class IpfsIosBackup < Formula
-    desc "Command line utility to backup iOS devices to an embedded and private IPFS node"
-    homepage "https://github.com/codynhat/ipfs-ios-backup"
-  
-    # Source code archive. Each tagged release will have one
-    url "https://github.com/codynhat/ipfs-ios-backup/archive/v0.2.0.tar.gz"
-    sha256 "a82dc8f7543e4eda4b1bf2f614d445258ef61b8551e51267d964173ff503b021"
-    head "https://github.com/codynhat/ipfs-ios-backup"
-  
-    depends_on "go" => :build
-    depends_on "codynhat/libimobiledevice/libimobiledevice"
-  
-    def install
-      ENV["GOPATH"] = buildpath
-  
-      bin_path = buildpath/"src/github.com/codynhat/ipfs-ios-backup"
+  desc "Backup iOS devices to an embedded and private IPFS node"
+  homepage "https://github.com/codynhat/ipfs-ios-backup"
 
-      bin_path.install Dir["*"]
-      cd bin_path do
-        # Install the compiled binary into Homebrew's `bin` - a pre-existing
-        # global variable
-        system "go", "build", "-o", bin/"ipfs-ios-backup", "."
-      end
+  # Source code archive. Each tagged release will have one
+  url "https://github.com/codynhat/ipfs-ios-backup/archive/v0.2.0.tar.gz"
+  sha256 "a82dc8f7543e4eda4b1bf2f614d445258ef61b8551e51267d964173ff503b021"
+  head "https://github.com/codynhat/ipfs-ios-backup"
+
+  depends_on "go" => :build
+  depends_on "codynhat/libimobiledevice/libimobiledevice"
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    bin_path = buildpath/"src/github.com/codynhat/ipfs-ios-backup"
+
+    bin_path.install Dir["*"]
+    cd bin_path do
+      # Install the compiled binary into Homebrew's `bin` - a pre-existing
+      # global variable
+      system "go", "build", "-o", bin/"ipfs-ios-backup", "."
     end
-  
-    test do
-      system bin/"ipfs-ios-backup", "--help"
-    end
+  end
 
-    plist_options :manual => "ipfs-ios-backup"
-
-    def plist; <<~EOS
+  plist_options :manual => "ipfs-ios-backup"
+  def plist
+    <<~EOS
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
       <plist version="1.0">
@@ -55,6 +51,10 @@ class IpfsIosBackup < Formula
           <string>#{var}/log/ipfs-ios-backup.log</string>
         </dict>
       </plist>
-      EOS
-    end
+    EOS
   end
+
+  test do
+    system bin/"ipfs-ios-backup", "--help"
+  end
+end
